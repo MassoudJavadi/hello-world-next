e9ce6598c4b113c48d80a3048c636dc777926979
Masoud Javadi <masoudjavadi180@gmail.com>
2021-12-29 23:19:27 +0330

    commit -m 'second-commit-tutorial-next-Max'

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,39 @@
-//domain.com
+import MeetupList from "../components/meetups/MeetupList";
+import Layout from "../components/layout/Layout";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "first meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "qom/2nd alley",
+    description: "This is the first meetup",
+  },
+  {
+    id: "m2",
+    title: "second meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "qom/4th alley",
+    description: "This is the second meetup",
+  },
+  {
+    id: "m3",
+    title: "third meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "qom/3rd alley",
+    description: "This is the third meetup",
+  },
+];
 
 const HomePage = () => {
-  return <div>HOME</div>;
+  return (
+    <Layout>
+      <MeetupList meetups={DUMMY_MEETUPS} />
+    </Layout>
+  );
 };
 
 export default HomePage;
